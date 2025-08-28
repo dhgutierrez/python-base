a5370dec4d3052cb64054714ea1af95fa717ec58
dhgutierrez <daniel.huanca@gmail.com>
2025-08-27 21:32:50 -0300

    Adicionado_Idiomas

--- hello.py
+++ hello.py
@@ -1,4 +1,44 @@
 #!/usr/bin/env python3
+""""Hello World Multi Linguas.
 
-# Este programa imprime Hello World
-print("hello, world!")
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem 
+correspondente.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+
+    export LANG-pt_BR
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Daniel Huanca Gutierrez"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+#snake case - current_language
+#Pascal Case - CurrentLanguage
+
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_ES":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour, le Monde!"
+elif current_language == "de_DE":
+    msg = "Hallo, Welt!"
+    
+print(msg)
